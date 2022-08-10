b60855d4a749fc4cef7719272deb041b7dac4695
KeitoH <ht20a082@oecu.jp>
2022-08-10 04:15:01 +0000

    Update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum:200 }
@@ -12,4 +13,7 @@ class Book < ApplicationRecord
     end
     image
   end
+
+  def favorited_by?(user)
+    favorites.exits?(user_id: user.id)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "home#top"
   get "home/about" => "home#about" , as: "about"
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
