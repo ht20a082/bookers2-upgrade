02d0b92ac5e843c942d47bdaf108bb49eaa2a46b
KeitoH <ht20a082@oecu.jp>
2022-08-10 04:00:05 +0000

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#top"
+  get "home/about" => "home#about" , as: "about"
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
