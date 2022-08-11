5eae97ba4b587ff0e75a4cdb9344233f891d0986
KeitoH <ht20a082@oecu.jp>
2022-08-11 09:17:27 +0000

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :book_comments, only: [:create, :destroy]
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
